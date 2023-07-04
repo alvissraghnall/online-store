cdd62439c23e0291bffb9b8f12370ae6d4be2670
Alviss Raghnall <isiomastewart@gmail.com>
2023-07-04 02:52:15 +0100

    Burna Boy - On The Low

--- src/components/dashboard/OrdersContent.vue
+++ src/components/dashboard/OrdersContent.vue
@@ -23,15 +23,18 @@
                         </div>
                     </th>
                     <th scope="col" class="text-left px-6 py-3">
-                        <div class="flex items-center">
+                        <div class="flex items-center cursor-pointer" @click="changeArrowDirection('status')">
                             Status
-                            <a 
-                                @click="changeArrowDirection('status')"
-                                class="w-3 h-3"
+                            <a
+                                class="w-1.5 h-1.5 flex items-center"
                             >
                             <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg> -->
                                 <OhVueIcon
-                                    name="ri-arrow-down-s-fill"
+                                    v-if="arrowDirections.status !== ArrowDirection.NEUTRAL"
+                                    :name="
+                                        arrowDirections.status === ArrowDirection.DOWN ? 'ri-arrow-down-s-fill'
+                                        : arrowDirections.status === ArrowDirection.UP ? 'ri-arrow-up-s-fill' : ''
+                                    "
                                 />
                             </a>
                         </div>
@@ -39,7 +42,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(order, idx) in orders" class="even:bg-gray-200 even:bg-opacity-30 last:border-b last:border-dotted last:border border-b dark:bg-gray-900 dark:border-gray-700">
+                <tr v-for="(order, idx) in sortedOrders" class="even:bg-gray-200 even:bg-opacity-30 last:border-b last:border-dotted last:border border-b dark:bg-gray-900 dark:border-gray-700">
                     <td class="px-6 py-4">
                         {{ idx + 1 }}
                     </td>
@@ -83,9 +86,18 @@ const props = defineProps<{
     orders: Order[]
 }>();
 
-const sortedOrders = ref(props.orders);
+const sortedOrders = ref<Order[]>([]);
+sortedOrders.value = [...props.orders];
+
+const statusOrderToBeSorted = {
+  [Order.status.CANCELLED]: 1,
+  [Order.status.PENDING]: 2,
+  [Order.status.PROCESSING]: 3,
+  [Order.status.COMPLETED]: 4,
+};
 
 const sortOrders = (field: keyof typeof arrowDirections) => {
+    console.log(props.orders, props.orders.map(r => r.status));
     switch (field) {
         case 'date':
             arrowDirections[field] === ArrowDirection.DOWN
@@ -109,17 +121,24 @@ const sortOrders = (field: keyof typeof arrowDirections) => {
                 : sortedOrders.value = props.orders;
 
         default:
-            arrowDirections[field] === ArrowDirection.DOWN
+            arrowDirections[field] === ArrowDirection.UP
                 ? sortedOrders.value.sort((a, b)  => {
-                    return a.status.toLowerCase() < b.status.toLowerCase() ? -1 : a.status.toLowerCase() > b.status.toLowerCase() ? 1 : 0;
+                    // return a.status === Order.status.CANCELLED ? -1 : a.status === Order.status.PENDING && b.status === Order.status.PROCESSING ? 1 : 0;
+                    // return Order.status[a.status] > Order.status[b.status] ? 1 : -1;
+                    return statusOrderToBeSorted[a.status] - statusOrderToBeSorted[b.status];
                 })
-                : arrowDirections[field] === ArrowDirection.UP
-                ? sortedOrders.value.sort((a, b)  => {
-                    return a.status.toLowerCase() < b.status.toLowerCase() ? -1 : a.status.toLowerCase() > b.status.toLowerCase() ? 1 : 0;
-                }).reverse()
-                : sortedOrders.value = props.orders;
+                : arrowDirections[field] === ArrowDirection.DOWN
+                ? sortedOrders.value.sort((a, b)  => statusOrderToBeSorted[a.status] - statusOrderToBeSorted[b.status]).reverse()
+                : sortedOrders.value.sort(
+                    (a, b) => { console.log(props.orders.map(s => s.status)); return props.orders.indexOf(a) - props.orders.indexOf(b)}
+                );
+            console.log(sortedOrders.value.map(r => r.status));
 
     }
+}
+
+const cancelSort = (a: Order, b: Order) => {
+
 }
 
 enum ArrowDirection {
